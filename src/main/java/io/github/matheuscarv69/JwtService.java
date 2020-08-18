55df0be571f7c64d39070e0bf68be7264d6583d2
matheuscarv69 <matheus9126@gmail.com>
2020-08-18 16:29:37 -0400

    Method of decoding the token and obtaining the user name of the implemented token

--- src/main/java/io/github/matheuscarv69/JwtService.java
+++ src/main/java/io/github/matheuscarv69/JwtService.java
@@ -1,9 +1,13 @@
 package io.github.matheuscarv69;
 
 import io.github.matheuscarv69.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -35,5 +39,35 @@ public class JwtService {
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
                 .compact();
     }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException{
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody(); // retorna as informações do token
+    }
+
+    public boolean tokenValido(String token){
+
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime dataHoraExpiracao = dataExpiracao
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+            return !LocalDateTime.now().isAfter(dataHoraExpiracao);
+            // se retornar true significa que o token é inválido, caso contrário ele ainda é válido
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
+    }
+
+    
 
 }
